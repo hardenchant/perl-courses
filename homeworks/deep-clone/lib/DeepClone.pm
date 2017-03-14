99bad5a66c1e464ccf480e21643ed4958c31a70d
lgv <lgv@lgv>
2017-03-14 23:43:24 +0300

    Наконец то рабочая версия /homework/deep-clone/

--- homeworks/deep-clone/lib/DeepClone.pm
+++ homeworks/deep-clone/lib/DeepClone.pm
@@ -33,64 +33,63 @@ use warnings;
 Элементами ссылок на массив или хеш, не могут быть ссылки на массивы и хеши исходной структуры данных.
 
 =cut
-
 sub clone {
 	my $orig = shift;
 	my $cloned;
+	my $linklist;
 
-	my $vl = shift;
-	$vl = 1 unless $vl;
-	my $success = 1;
+	my $context = 0;
+	my $subfail = 0;
 
-	my $origg = shift;
-	$origg = $orig unless $origg;
+	if (scalar @_ > 0){
+		$linklist = shift;
+		$context = 1;
+	}
+	else
+	{
+		$linklist = {};
+	}
+
 	# ...
 	# deep clone algorith here
 	if (ref $orig eq "ARRAY"){
-		if ($#{$orig} == -1){
-			$cloned = [];
-		}
-		else
-		{
-			my @newarr;
-			for my $i (@$orig){
-				if((ref $i eq "ARRAY") && (($i eq $orig) || ($i eq $origg))) # 
-				{
-					$success = 0;
+		$cloned = [];
+		%$linklist = (%$linklist, $orig, \@$cloned);
+		for my $i (@$orig){
+			my $success = 0;
+			for my $k (keys %$linklist){
+				if  ($i && $i eq $k){
+					push @$cloned, $linklist->{$k};
+					$success = 1;
 					last;
 				}
-				else
-				{
-					my @temp = clone($i, $vl + 1, $origg);
-					$success = $temp[2];
-					push @newarr, $temp[0];
-				}
 			}
-			$cloned = \@newarr;
+			unless ($success){
+				my @temp = clone($i, $linklist);
+				$subfail = $temp[1];
+				last if ($subfail);
+				push @$cloned, $temp[0];
+			} 
 		}
 	}
 	elsif (ref $orig eq "HASH"){
-		if (scalar keys %$orig == 0){
-			$cloned = {};
-		}
-		else
-		{
-			my %newhash;
-			for my $i (keys %$orig){
-				if((ref $orig->{$i} eq "HASH") && (($orig->{$i} eq $orig) || ($orig->{$i} eq $origg))){ #  
-					$success = 0;
+		$cloned = {};
+		%$linklist = (%$linklist, $orig, \%$cloned);
+		for my $i (keys %$orig){
+			my $success = 0;
+			for my $k (keys %$linklist){
+				if ($orig->{$i} && $orig->{$i} eq $k){
+					%$cloned = (%$cloned, $i, $linklist->{$k});
+					$success = 1;
 					last;
 				}
-				else
-				{
-					my @temp = clone($orig->{$i}, $vl + 1, $origg);
-					$success = $temp[2];
-					%newhash = (%newhash, $i, $temp[0]);
-					
-				}
 			}
-
-			$cloned = \%newhash;
+			unless ($success){
+				my @temp = clone($orig->{$i}, $linklist);
+				$subfail = $temp[1];
+				last if ($subfail);
+				%$cloned = (%$cloned, $i, $temp[0]);
+			}
 		}
 	}
 	elsif(ref \$orig eq "SCALAR")
@@ -99,22 +98,17 @@ sub clone {
 	}
 	else
 	{
-		$success = 0;
+		$subfail = 1;
 	}
 
 
-	if ($vl == 1){
-		if ($success == 1){
-			return $cloned;
-		}
-		else
-		{
-			return undef;
-		}
+	if ($context){
+		return $cloned, $subfail;
 	}
 	else
-	{
-		return $cloned, $vl, $success;
+	{	
+		return undef if($subfail);
+		return $cloned;
 	}
 }
 1;
