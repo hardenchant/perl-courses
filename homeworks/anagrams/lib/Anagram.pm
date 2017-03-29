842997867313bbae5b4c1f1e26ef4f6138cea173
lgv <lgv@lgv>
2017-03-29 05:24:55 +0300

    Make faster anagrams

--- homeworks/anagrams/lib/Anagram.pm
+++ homeworks/anagrams/lib/Anagram.pm
@@ -40,39 +40,31 @@ anagram(['пятак', 'ЛиСток', 'пятка', 'стул', 'ПяТаК', '
 =cut
 use utf8;
 use Encode qw(decode_utf8 encode_utf8);
-
 sub anagram {
     my $words_list = shift;
     my %result;
     #    
     # Поиск анограмм map {encode_utf8(lc decode_utf8($_))}
    	my %temphash;
-   	my @temparr;
    	for my $i (@$words_list){
    		my $word = encode_utf8(lc decode_utf8($i));
-   		if ($temphash{length($word)}{$word}){
+   		my $keyword = join "", sort split "", $word;
+   		if ($temphash{length($word)}{$keyword}{words}{$word}){
    			next;
    		}
    		else
    		{
-   			push @temparr, $word;
-   			$temphash{length($word)}{$word} = join "", sort split "", $word;
-   		}
-   	}
-   	for my $word (@temparr){
-   		next unless ($temphash{length($word)}{$word});
-   		for my $k (keys %{$temphash{length($word)}}){
-   			if ($temphash{length($word)}{$word} eq $temphash{length($word)}{$k}){
-   				push @{$result{$word}}, $k;
-   				delete $temphash{length($word)}{$k} unless $word eq $k;
+   			$temphash{length($word)}{$keyword}{words}{$word}++;
+   			unless ($temphash{length($word)}{$keyword}{first}){
+   				$temphash{length($word)}{$keyword}{first} = $word;
    			}
    		}
-   		unless($#{$result{$word}}){
-   			delete $result{$word};
-   		}
-   		else
-   		{
-   			@{$result{$word}} = sort @{$result{$word}};
+   	}
+   	for my $len (keys %temphash){
+   		for my $keyword (keys %{$temphash{$len}}){
+   			if( scalar keys %{$temphash{$len}{$keyword}{words}} > 1){
+   				$result{$temphash{$len}{$keyword}{first}} = [sort keys %{$temphash{$len}{$keyword}{words}}];
+   			}
    		}
    	}
 
