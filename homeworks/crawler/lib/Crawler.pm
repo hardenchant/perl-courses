c90bf02852cb4294485e057b6ae5d75c85b2b6ce
Gleb Lysov <barabashka.zzz@gmail.com>
2017-05-12 02:09:46 +0300

    YEAH CRAWLER

--- homeworks/crawler/lib/Crawler.pm
+++ homeworks/crawler/lib/Crawler.pm
@@ -51,6 +51,56 @@ sub run {
     #Код crawler-а
     #............
 
+	$AnyEvent::HTTP::MAX_PER_HOST = $parallel_factor;
+
+	my $cv = AE::cv();
+	my $url = URI->new($start_page)->canonical;
+
+	my %result;
+	my %visited;
+
+	my $func; $func = sub {
+		my $site = shift;
+		my $cond = shift;
+		$cv->begin;
+		if ($cond) {
+			#head
+			return unless (scalar keys %result < 1000);
+			http_head $site, sub {
+				shift; my $hdr = shift;
+				if ($hdr->{'content-type'} =~ m/.*text\/html.*/){
+					$func->($site);
+				}
+				$cv->end;
+			};
+		}
+		else {
+			#get
+			return unless (scalar keys %result < 1000);
+			http_get $site, sub {
+				my ($body, $hdr) = @_;
+				$result{$site} = $hdr->{'content-length'};
+				wq($body)->find('a')->each(sub {
+					shift; #skip index
+					my $uri = URI->new($_->attr('href'))->canonical->abs($url);
+					if ($uri =~ m{^$url.*}){
+						$uri = URI->new($uri->path())->abs($url);#исключаем теги привязки из ссылок
+						unless ($visited{$uri}) {
+							$visited{$uri}++;
+							$func->($uri, 1);
+						}
+					}
+				});
+				$cv->end;
+			};
+		}
+	}; $func->($url);
+	$cv->recv;
+	$total_size += $_ for (values %result);
+	my @sort_key = (sort { $result{$b} <=> $result{$a} } keys %result);
+	for (1..10){
+		push @top10_list, shift @sort_key;
+	}
     return $total_size, @top10_list;
 }
 
