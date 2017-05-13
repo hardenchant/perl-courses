621e75b3f38b3f60a141ccc18c4b2e1f2d798fee
Gleb Lysov <barabashka.zzz@gmail.com>
2017-05-13 13:15:25 +0300

    Add save_stdin homework

--- homeworks/save_stdin/bin/stdin.pl
+++ homeworks/save_stdin/bin/stdin.pl
@@ -4,22 +4,32 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-
-my $file = "out.a";
+my $file = "a.out";
 GetOptions('file=s' => \$file);
 my $count_sigint = 0;
-$SIG{INT} = sub{
-	unless($count_sigint){
-		print STDERR "Double Ctrl+C for exit\n";
+$SIG{INT} = sub {
+	unless ($count_sigint) {
+		print STDERR "Double Ctrl+C for exit";
 	}
-	else{
+	else {
 		close(STDIN);
 	}
 	$count_sigint++;
 };
-open(my $fh, ">", $file) or die $!;
-select($fh);
-do {
-	print <>;
-} while(!eof() && !$count_sigint);
+open (my $fh, ">", $file) or die $!;
+print "Get ready\n";
+
+my $olds = select $fh;
+my ($size, $lines, $middle_length) = (0, 0, 0);
+
+while (my $line = <>) {
+	print $line;
+	$lines++;
+	chomp $line;
+	$size += length $line;
+}
 close $fh;
+select $olds;
+$middle_length = $lines ? int $size/$lines : 0;
+
+print "$size $lines $middle_length\n";
