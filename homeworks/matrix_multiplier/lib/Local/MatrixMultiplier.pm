2699a67a8055e34d065a1534b7b85c5a20658e9c
Gleb Lysov <barabashka.zzz@gmail.com>
2017-05-14 16:22:00 +0300

    Matrix multiplier complete

--- homeworks/matrix_multiplier/lib/Local/MatrixMultiplier.pm
+++ homeworks/matrix_multiplier/lib/Local/MatrixMultiplier.pm
@@ -2,40 +2,73 @@ package Local::MatrixMultiplier;
 
 use strict;
 use warnings;
+use threads;
+use threads::shared;
+
 
 sub mult {
 	my ($mat_a, $mat_b, $max_child) = @_;
 	my $res = [];
+	for my $row (@{$mat_a}){
+		die "Wrong matrix" if (scalar @{@{$mat_a}[0]}) != (scalar @{$row});
+	}
+	for my $row (@{$mat_b}){
+		die "Wrong matrix" if (scalar @{@{$mat_b}[0]}) != (scalar @{$row});
+	}
+	die "Wrong matrix" unless scalar @{@{$mat_a}[0]} == scalar @{$mat_b};
 
-	my ($read, $write);
-	pipe ($read, $write);
-
-	my $potoks = 1;	#2
-	my @procs;
+	my @res_pos = (scalar @$mat_a, scalar @{@{$mat_b}[0]}, scalar @$mat_b - 1);
 	
-	my @arr = (1,2,3,4,5,6,7,8,9,10);
-	
-	for (1..$potoks){
-		if (my $ppid = fork()){
-			push @procs, $ppid;
-		}
-		else
-		{
-			close $read;
-			print "i'm CHILD";
-			print $write "HELLO";
-			exit;
-		}
+	my @result;
+	for (0..$res_pos[2]){
+		$result[$_] = [];
 	}
-	close $write;
-	print join "-", @procs;
-	for my $pid (@procs){
-		waitpid($pid, 0);
-	}
-
-	while(<$read>){
-		print $_;
-	}
+	my $ref_res = shared_clone(\@result);
+	my @pos:shared = (0, 0);
+	my $ref_pos = \@pos;
+	my @threads;
+	push @threads, threads->create(sub {
+					my @local_pos;
+					{
+						lock($ref_pos);
+						@local_pos = @pos;
+						if (@pos){
+							if ($pos[1] + 1 < $res_pos[1]) {
+								$pos[1]++;
+							}
+							elsif ($pos[0] + 1 < $res_pos[0]) {
+								$pos[0]++;
+								$pos[1] = 0;
+							}
+							else {
+								@pos = ();
+							}
+						}
+					}
+					while (@local_pos) {
+						for (0..$res_pos[2]){
+							$ref_res->[$local_pos[0]]->[$local_pos[1]] += @{@{$mat_a}[$local_pos[0]]}[$_] * @{@{$mat_b}[$_]}[$local_pos[1]];
+						}
+						{
+							lock($ref_pos);
+							@local_pos = @pos;
+							if (@pos){
+								if ($pos[1] + 1 < $res_pos[1]) {
+									$pos[1]++;
+								}
+								elsif ($pos[0] + 1 < $res_pos[0]) {
+									$pos[0]++;
+									$pos[1] = 0;
+								}
+								else {
+									@pos = ();
+								}
+							}
+						}
+					}
+								       }) for (1..$max_child); 
+ 	$_->join() for (@threads);
+	$res = $ref_res;
 	return $res;
 }
 
