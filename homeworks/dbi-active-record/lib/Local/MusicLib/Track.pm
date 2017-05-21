69d99a7e5e7e1aba38686bc835431f5f881bb59e
Gleb Lysov <barabashka.zzz@gmail.com>
2017-05-21 14:57:32 +0300

    musiclib::track is done

---
+++ homeworks/dbi-active-record/lib/Local/MusicLib/Track.pm
@@ -0,0 +1,50 @@
+package Local::MusicLib::Track;
+
+use DBI::ActiveRecord;
+use Local::MusicLib::DB::MySQL;
+
+use DateTime;
+
+db "Local::MusicLib::DB::MySQL";
+
+table 'tracks';
+
+has_field id => (
+	isa => 'Int',
+	auto_increment => 1,
+	index => 'primary',
+);
+
+has_field album_id => (
+    isa => 'Int',
+    index => 'common',
+    default_limit => 100,
+);
+
+has_field name => (
+    isa => 'Str',
+    index => 'common',
+    default_limit => 100,
+);
+
+has_field duration => (
+    isa => 'Str',
+    serializer => sub {my @t = split ":", $_;
+    				   $t[0] * 3600 + $t[1] * 60 + $t[2];},
+    deserializer => sub {my $hh = int $_ / 3600;
+    					 my $mm = int ($_ - $hh * 3600) / 60;
+    					 my $ss = $_ - $hh * 3600 - $mm * 60;
+						 "$hh:$mm:$ss"},
+);
+
+has_field create_time => (
+    isa => 'DateTime',
+    serializer => sub { $_[0]->epoch },
+    deserializer => sub { DateTime->from_epoch(epoch => $_[0]) },
+);
+
+
+no DBI::ActiveRecord;
+__PACKAGE__->meta->make_immutable();
+
+1;
